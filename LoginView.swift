e40f42966a5cbd8356d233a481fffcd03f1b24fe
Peter Roumeliotis <101145565+PeterRoumeliotis@users.noreply.github.com>
2024-12-09 01:33:27 -0500

    added login firebase func

--- LoginView.swift
+++ LoginView.swift
@@ -3,12 +3,17 @@
 //  Peter Roumeliotis
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
 
 //Login View
 struct LoginView: View {
     @EnvironmentObject var session: SessionManager
     @State private var email = ""
     @State private var password = ""
+    @State var showError: Bool = false
+    @State var errorMessage: String = ""
+    
     var body: some View {
         VStack {
             Spacer()
@@ -25,8 +30,7 @@ struct LoginView: View {
                 .padding()
                 .textFieldStyle(RoundedBorderTextFieldStyle())
             Button(action: {
-                // Implement login logic
-                session.isLoggedIn = true
+                loginUser()
             }) {
                 Text("Sign In")
                     .frame(maxWidth: .infinity)
@@ -39,5 +43,39 @@ struct LoginView: View {
             Spacer()
         }
         .padding()
+        .alert(errorMessage, isPresented: $showError, actions: {})
     }
+    
+    func loginUser(){
+        Task{
+            do{
+                try await Auth.auth().signIn(withEmail: email, password: password)
+                print("User Found")
+                session.isLoggedIn = true
+            } catch {
+                await setError(error)
+            }
+            
+        }
+    }
+    
+    func setError(_ error: Error) async {
+        await MainActor.run {
+            let authError = error as NSError
+            switch authError.code {
+            case AuthErrorCode.invalidEmail.rawValue:
+                errorMessage = "The email address is invalid. Please try again."
+            case AuthErrorCode.wrongPassword.rawValue:
+                errorMessage = "The password is incorrect. Please try again."
+            case AuthErrorCode.userNotFound.rawValue:
+                errorMessage = "No user found with this email. Please check your credentials."
+            default:
+                errorMessage = "Invalid login. Please check your email and password."
+            }
+            showError.toggle()
+        }
+    }
+
+    
 }
+
