c91496d6a0e5fdb56bef23797809cf354e27be6b
Peter Roumeliotis <101145565+PeterRoumeliotis@users.noreply.github.com>
2024-12-13 20:14:24 -0500

    Update Post.swift

--- Post.swift
+++ Post.swift
@@ -6,4 +6,6 @@ struct Post: Identifiable {
     var timestamp: Date
     var authorName: String
     var authorSubtitle: String
+    var likeCount: Int
+    var authorID: String // New field to identify the author
 }
