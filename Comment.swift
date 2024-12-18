9ecc333cc1ae384a7158c8905bb86b01eca33a95
Peter Roumeliotis <101145565+PeterRoumeliotis@users.noreply.github.com>
2024-12-17 22:50:22 -0500

    Update Comment.swift

--- Comment.swift
+++ Comment.swift
@@ -1,8 +1,15 @@
+//  Comment.swift
+//  PhiDeltConnectV2
+//  Peter Roumeliotis
+
 import Foundation
 
+// Comment is one comment on a post
+
 struct Comment: Identifiable {
-    var id: String
+    var id: String //Firestore doc ID
     var content: String
     var timestamp: Date
     var authorName: String
+    var profilePicName: String
 }
