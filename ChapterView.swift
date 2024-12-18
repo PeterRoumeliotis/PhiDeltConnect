03df180dd670b1af3b8f05e1ec57bd1b71d8a399
Peter Roumeliotis <101145565+PeterRoumeliotis@users.noreply.github.com>
2024-12-17 22:50:04 -0500

    Update ChapterView.swift

--- ChapterView.swift
+++ ChapterView.swift
@@ -5,25 +5,31 @@
 import SwiftUI
 import WebKit
 
-//Chapter View
+// A View that shows a WebView with the PhiDelt website.
+
 struct ChapterView: View {
     var body: some View {
+        // Gives the WebView with a given URL
         WebView(url: URL(string: "https://portal.phideltatheta.org")!)
-            .edgesIgnoringSafeArea(.all)
+            .edgesIgnoringSafeArea(.all) // Makes it full screen
     }
 }
 
-// WebView Wrapper for WKWebView
+// Need UIViewRepresentable to show the webview
 struct WebView: UIViewRepresentable {
+    // Need updateUIView or it won't work
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+    }
+    
     let url: URL
     
     func makeUIView(context: Context) -> WKWebView {
+        // Create a WKWebView instance
         let webView = WKWebView()
         let request = URLRequest(url: url)
+        // Load it to display the webpage
         webView.load(request)
         return webView
     }
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        // Update the view
-    }
+
 }
