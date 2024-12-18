22333da7b4c77c223e9fd4b6a41ee8dccebd0544
Peter Roumeliotis <101145565+PeterRoumeliotis@users.noreply.github.com>
2024-12-17 22:51:51 -0500

    Update NotificationItem.swift

--- NotificationItem.swift
+++ NotificationItem.swift
@@ -1,11 +1,17 @@
+// NotificationItem.swift
+// PhiDeltConnectV2
+// Peter Roumeliotis
+
 import Foundation
+
+// A notification
 
 struct NotificationItem: Identifiable {
     var id: String
-    var type: String      // "like" or "comment"
+    var type: String // Like, Comment, or Follow
     var postID: String
     var senderUserID: String
     var recipientUserID: String
     var timestamp: Date
-    var senderName: String = "" // New field for sender's profile name
+    var senderName: String = ""
 }
