9faa42a61d3a4e45770019569ec7b92b3a43409e
Peter Roumeliotis <101145565+PeterRoumeliotis@users.noreply.github.com>
2024-12-17 22:53:37 -0500

    Update Profile.swift

--- Profile.swift
+++ Profile.swift
@@ -1,4 +1,10 @@
+//  Profile.swift
+//  PhiDeltConnectV2
+//  Peter Roumeliotis
+
 import Foundation
+
+// Profile is the user profile data.
 
 struct Profile: Codable {
     var name: String
@@ -6,4 +12,7 @@ struct Profile: Codable {
     var about: String
     var experience: String
     var yearsInFraternity: Int
+    var profilePicName: String
+    var followers: [String] //Array of userIDs that follow the person
+    var lookingForWorkTitle: String = "" //A field to put a job you are looking for
 }
