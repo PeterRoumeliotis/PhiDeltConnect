d9039462aedf420c283fb1ddeb14f1e0f57eeaa2
Peter Roumeliotis <101145565+PeterRoumeliotis@users.noreply.github.com>
2024-12-17 22:52:49 -0500

    Update Post.swift

--- Post.swift
+++ Post.swift
@@ -1,11 +1,19 @@
+//  Post.swift
+//  PhiDeltConnectV2
+//  Peter Roumeliotis
+
 import Foundation
 
+// Post is a single post.
+
 struct Post: Identifiable {
-    var id: String
+    var id: String //Post id
     var content: String
     var timestamp: Date
     var authorName: String
     var authorSubtitle: String
     var likeCount: Int
-    var authorID: String // New field to identify the author
+    var authorID: String //UserID
+    var profilePicName: String
+    var likedBy: [String] //Array of people who liked post
 }
